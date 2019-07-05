98ef75e8b5bfddb4ded18acbef570e26a8915fce
Le Tam <letam.jane@gmail.com>
2019-07-05 09:56:57 +0200

    Project set up like CBP

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Message from './message/message.jsx';
 import './index.scss';
 import './index.html';
 
@@ -9,9 +8,6 @@ class Hello extends React.Component {
     return (
       <>
         <h1>Hello world</h1>
-        <Message title="My title" body="hello body" />
-        <Message title="My second title" body="body body body" />
-        <Message title="Last title" body="show me your body" />
       </>
     );
   }
